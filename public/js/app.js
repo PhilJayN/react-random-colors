08c2d0f3290317b6b2215e7e9ba8b4e84a99abf6
Philip N <phi.nguyen@laverne.edu>
2018-05-06 00:08:56 -0700

    Show color code to UI with props

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,50 @@
 
 // Parent Component
 class ColorsDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: [],
+    };
+
+  }
+
+  componentDidMount() {
+    this.setState({ colors: Seed.colors })
+  }
+
   render() {
+
+    const colors = this.state.colors.map((color) =>(
+      <Color
+        key={'color-' + color.id}
+        code={color.code}
+      />
+    ));
+
     return(
       <div className="content-wrapper">
-        <ColorsController />
-        <SelectableColors />
+        {colors}
       </div>
     );
   }
 
 }
 //child Component
-class SelectableColors extends React.Component {
+class Color extends React.Component {
+
   render() {
+    // const style = {
+    //   // backgroundColor: 'goldenrod',
+    //   backgroundColor: 'pink',
+    // }
     return(
       <div className="colors-wrapper">
-        <div className="color">
-          {helpers.newColor()};
+        <div
+          className="color"
+        >
+          {this.props.code}
         </div>
       </div>
     );
@@ -26,6 +53,7 @@ class SelectableColors extends React.Component {
 
 }
 
+//button component
 class ColorsController extends React.Component {
   handleNewColors () {
     console.log('new colors btn clicked!!');
@@ -43,8 +71,9 @@ class ColorsController extends React.Component {
       </div>
     );
   }
-
 }
+
+
 ReactDOM.render(
   <ColorsDashboard />,
   document.getElementById('content')
