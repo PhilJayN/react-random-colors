1475a7eb13f2c32f28f0935477566718420500fe
Philip N <phi.nguyen@laverne.edu>
2018-05-08 16:16:06 -0700

    Refactor generateColors

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -90,19 +90,48 @@ window.helpers = (function () {
 
   }
 
+  function decideBounds(hue) {
+    var rMax, rMin, gMax, gMin, bMax, bMin;
+    //this fxn has hard coded color bounds
+    if (hue === 'Midnight Dream') {
+      rMax = 48,
+      rMin = 21,
+      gMax = 40,
+      gMin = 4,
+      bMax = 86,
+      bMin = 71;
+    }
+    else if (hue === 'Summerset Sunshine') {
+      rMax = 248,
+      rMin = 242,
+      gMax = 218,
+      gMin = 209,
+      bMax = 60,
+      bMin = 2;
+    }
+    return {
+      rMax: rMax,
+      rMin: rMin,
+      gMax: gMax,
+      gMin: gMin,
+      bMax: bMax,
+      bMin: bMin,
+    }
+  }
+
   function generateColors(hue) {
-    if (hue === 'Summerset Sunshine') {
-      return hueYellow();
-    }
-    else if (hue === 'Sunday Pastels') {
-      return pastels();
-    }
-    else if (hue === 'Midnight Dream') {
-      return hueMidnight();
-    } else {
-      var r = Math.round( Math.random() * (48 - 21) + 21 );
-      var g = Math.round( Math.random() * (40 - 4) + 4 );
-      var b = Math.round( Math.random() * (86 - 71) + 71 );
+    var b = decideBounds(hue);
+
+    // if (hue === 'Summerset Sunshine') {
+    //   return hueYellow();
+    // }
+    // else if (hue === 'Sunday Pastels') {
+    //   return pastels();
+    // }
+    // (max - min) + min
+      var r = Math.round( Math.random() * (b.rMax - b.rMin) + b.rMin );
+      var g = Math.round( Math.random() * (b.gMax - b.gMin) + b.gMin );
+      var b = Math.round( Math.random() * (b.bMax - b.bMin) + b.bMin );
 
       var color = 'rgb('
       + r.toString()
@@ -114,8 +143,6 @@ window.helpers = (function () {
 
       console.log ('r', r, 'g', g, 'b', b, 'color var:', color);
       return color;
-    }
-
   }
 
 
@@ -128,5 +155,6 @@ window.helpers = (function () {
     hueYellow,
     hueMidnight,
     generateColors,
+    decideBounds,
   };
 }());
